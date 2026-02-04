caec56cc2a0bc22cc2afd86ec191fe4c3ba79da0
magyariarnold3 <magyariarnold3@gmail.com>
2026-02-04 09:54:44 +0200

    code for uploading files to the raspberry, compatible with windows systems

--- src/ble_remote.py
+++ src/ble_remote.py
@@ -10,6 +10,7 @@ def find_device_by_name(target_name):
             return device
     return None
 
+
 def is_increasing(values):
     count = 0
     for i in range(1, len(values)):
@@ -18,6 +19,7 @@ def is_increasing(values):
     if count >= len(values) / 2:
         return True
     return False
+
 
 def is_declining(values):
     count = 0
@@ -28,44 +30,56 @@ def is_declining(values):
         return True
     return False
 
+
 def decode_event(events, code):
     values: list[int] = [event.value for event in events]
     print(f"Values: {values}")
-    if code == 54:
-        if is_increasing(values):
-            print("Up")
-        elif is_declining(values):
-            print("Down")
-    elif code == 53:
-        if is_increasing(values):
-            print("Left")
-        elif is_declining(values):
-            print("Right")
-    elif code == 57:
-        print("Bottom Button Pressed")
+    if len(values) > 2:
+        if code == 54:
+            if is_increasing(values):
+                print("Up")
+            elif is_declining(values):
+                print("Down")
+        elif code == 53:
+            if is_increasing(values):
+                print("Left")
+            elif is_declining(values):
+                print("Right")
+    else:
+        if code == 57:
+            print("Bottom Button Pressed")
+
 
 def main():
-    device = None
-    while device is None:
-        device = find_device_by_name(TARGET_NAME)
-        if device is None:
-            print(f"Device '{TARGET_NAME}' not found. Please connect the device and press Enter to retry.")
-            time.sleep(1)
+    while True:
+        try:
+            device = None
+            while device is None:
+                device = find_device_by_name(TARGET_NAME)
+                if device is None:
+                    print(f"Device '{TARGET_NAME}' not found. Please connect the device and press Enter to retry.")
+                    time.sleep(1)
 
-    current_batch: list = []
-    code = 0
-    for event in device.read_loop():
-        if event.type == evdev.ecodes.EV_ABS:
-            # print(f"Code: {event.code} - {event.value}")
-            if event.code in [53, 54]:
-                code = event.code
-                current_batch.append(event)
-            elif event.code == 57 and event.value == -1:
-                if len(current_batch) > 0:
-                    decode_event(current_batch, code)
-                    current_batch = []
-                    code = 0
+            current_batch: list = []
+            code = 0
+            for event in device.read_loop():
+                if event.type == evdev.ecodes.EV_ABS:
+                    print(f"Code: {event.code} - {event.value}")
+                    if event.code in [53, 54, 57]:
+                        code = event.code
+                        current_batch.append(event)
+                    if event.code == 57 and event.value == -1:
+                        if len(current_batch) > 0:
+                            decode_event(current_batch, code)
+                            current_batch = []
+                            code = 0
+        except OSError:
+            print("The device is disconnected")
+            time.sleep(3)
+        except Exception as error:
+            print(f"An unexpected error has occured: {error}")
+            time.sleep(3)
+
 
 if __name__ == "__main__":
     main()
-
