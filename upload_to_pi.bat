867f1d3ecf0879b05cad73621260b452f755a7c7
magyariarnold3 <magyariarnold3@gmail.com>
2026-02-04 09:54:13 +0200

    code for uploading files to the raspberry, compatible with windows systems

--- upload_to_pi.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set RPI_HOST=pi@192.168.0.112
-set RPI_PATH=/home/pi/ScreenInTheBottle
-set LOCAL_PATH=C:\Arni\MyProjects\ScreenInTheBottle\src
-
-scp -r "%LOCAL_PATH%\*" %RPI_HOST%:%RPI_PATH%
